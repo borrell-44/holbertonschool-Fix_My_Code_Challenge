27e3e6ccf18deb01d5a6b0c6bd64ebe050a2bc2a
borrell-44 <alexanderborrell44@hotmail.com>
2022-09-30 08:34:28 -0400

    Hot Fix

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -30,7 +30,7 @@ class Square():
 
 if __name__ == "__main__":
 
-    s = Square(width=3, height=3)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.permiterOfMySquare())
