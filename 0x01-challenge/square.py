e8fa3cbb494669be1e521c4bda00dad7913355c9
borrell-44 <alexanderborrell44@hotmail.com>
2022-09-30 09:02:10 -0400

    Hot Fix

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -4,9 +4,11 @@
 
 
 class Square():
+
     """Square"""
 
     width = 0
+    height = 0
 
     def __init__(self, *args, **kwargs):
         """init"""
@@ -15,20 +17,20 @@ class Square():
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
     def permiterOfMySquare(self):
         """Perimeter"""
-        return (self.width * 2) + (self.width * 2)
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
         """String"""
-        return "{}/{}".format(self.width, self.width)
+        return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
 
-    s = Square(width=12)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.permiterOfMySquare())
